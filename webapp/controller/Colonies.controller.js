a7dc45f2cdcd70d75282417b47cc66916eb30915
Dauenhauer Evgenij <evgenij.dauenhauer@hotmail.com>
2018-02-03 20:14:29 +0000

    data binding works

--- webapp/controller/Colonies.controller.js
+++ webapp/controller/Colonies.controller.js
@@ -1,34 +1,23 @@
 sap.ui.define([
-	"./BaseController"
-], function(BaseController) {
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
 	"use strict";
 
-	return BaseController.extend("BeeApp.controller.home", {
+	return Controller.extend("BeeApp.controller.Colonies", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf BeeApp.view.home
+		 * @memberOf BeeApp.view.Colonies
 		 */
 			onInit: function() {
 		
-			},
-			
-			handelPressColonies: function(evt) {
-			//jQuery.sap.log.error("This should never have happened!");
-			//jQuery.sap.log.info("Pressed");
-			var oItem, oCtx;
-			oItem = evt.getSource();
-			oCtx = oItem.getBindingContext();
-			//alert("Navigating to Details of " + evt.getSource().getBindingContext());
-			this.getRouter().navTo("Colonies");
-				
 			}
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf BeeApp.view.home
+		 * @memberOf BeeApp.view.Colonies
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -37,7 +26,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf BeeApp.view.home
+		 * @memberOf BeeApp.view.Colonies
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -45,7 +34,7 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf BeeApp.view.home
+		 * @memberOf BeeApp.view.Colonies
 		 */
 		//	onExit: function() {
 		//
