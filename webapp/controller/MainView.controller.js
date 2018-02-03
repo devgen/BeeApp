af0ee1baebcab7de90c04e719bbd0244114b79a5
Dauenhauer Evgenij <evgenij.dauenhauer@hotmail.com>
2018-02-03 17:58:22 +0000

    first commit

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	
+	return Controller.extend("BeeApp.controller.MainView", {
+		init: function() {
+
+		},
+		onOpenDialog : function () {
+			this.getOwnerComponent().openAirlineCreationDeialog();
+		}
+
+	});
+});
